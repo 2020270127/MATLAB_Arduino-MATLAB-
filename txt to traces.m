960ed2a8ceafd6a9824e3f85c501728cafc32ed6
2020270127 <69888585+2020270127@users.noreply.github.com>
2020-09-10 03:24:19 +0900

    Update txt to traces.m

--- txt to traces.m
+++ txt to traces.m
@@ -1,30 +1,9 @@
-trace = load('waveform00001.txt');
+trace = load('waveform05d.txt');
 
-for i=2:9
-    
-  data = load(sprintf('waveform0000%d.txt', i));
-  trace= horzcat(trace, data); %updating trace var
-  disp(i)
- 
-end
-
-for i=10:99
-   
-  data = load(sprintf('waveform000%d.txt', i));
-  trace= horzcat(trace, data);  
-  disp(i)
-end
-
-for i=100:999
-  data = load(sprintf('waveform00%d.txt', i));
-  trace= horzcat(trace, data); 
-  disp(i)
-end
-
-
-  data = load('waveform01000.txt');
-  trace= horzcat(trace, data);  
-  
- save('AES.traces', 'trace', '-ascii'); %save as ascii
- 
+for i=1:1000
+    {
+    data = load(sprintf('waveform%05d.txt', i));
+    trace= horzcat(trace, data);  
+    save('AES.traces', 'trace', '-ascii'); %save as ascii
+} 
  %MATLAB
